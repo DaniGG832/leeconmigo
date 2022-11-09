e0ebdcb2e171ffece098fda16cb72466ee6bfdbf
Daniel GG <daniel.gonzalez.garcia@iesdonana.org>
2022-11-09 17:22:13 +0100

    rutas auth

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,56 @@ Route::get('/dashboard', function () {
 require __DIR__.'/auth.php';
 
 
+
+
+
+
+
+Route::middleware(['auth'])->group(function () {
+    
+    Route::get('index',[LibroController::class, 'index'] )->name('libro.index');
+    Route::get('show',[LibroController::class, 'show'] )->name('libro.show');
+    
+
+    
+});
+
+
+
+Route::middleware(['auth', 'can:solo-admin'])->group(function () {
+    
+    Route::prefix('admin')->group(function () {
+        
+            Route::get('prueba',[RolController::class, 'index'] )->name('prueba');
+        
+            
+            Route::get('libro-create',[LibroController::class, 'create'] )->name('libro.create');
+            Route::post('libro-store',[LibroController::class, 'store'] )->name('libro.store');
+
+            Route::get('prueba',[RolController::class, 'index'] )->name('prueba');
+
+
+        });
+    
+
+    //Route::get('prueba',[LibroController::class, 'index'] )->name('prueba');
+    
+
+    
+});
+
+Route::middleware(['auth', 'can:solo-superadmin'])->group(function () {
+    
+    
+    //Route::get('prueba',[RolController::class, 'index'] )->name('prueba');
+
+
+
+});
+
+
+/* Route::get('prueba', function () {
+
+    return view('prueba');
+}); */
+
