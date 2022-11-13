904e5db0359a1d9b6567049e5ae9dabc653dcdd8
Daniel GG <daniel.gonzalez.garcia@iesdonana.org>
2022-11-13 13:24:50 +0100

    vista admin index

--- resources/views/admin/libros/index.blade.php
+++ resources/views/admin/libros/index.blade.php
@@ -62,7 +62,7 @@
                         <tr>
                           <td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
                             <div class="flex items-center">
-                              <div class="flex-shrink-0 w-10 h-10"><img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt=""></div>
+                              <div class="flex-shrink-0 w-10 h-10"><img class="w-10 h-12 rounded-lg" src="{{asset('img/el-principito.jpg')}}" alt=""></div>
                               <div class="ml-4">
                                 <div class="text-sm font-medium leading-5 text-gray-900">{{$libro->titulo}}</div>
                                 <div class="text-sm leading-5 text-gray-500">{{$libro->titulo_original}}</div>
