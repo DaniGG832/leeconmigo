6c674c8e5d6fef6d5bd019cbbb39d673372c9759
Daniel GG <daniel.gonzalez.garcia@iesdonana.org>
2022-11-20 19:07:00 +0100

    añadir enlaces temas index (imagen y nombre)

--- resources/views/admin/temas/index.blade.php
+++ resources/views/admin/temas/index.blade.php
@@ -65,9 +65,9 @@
                         <tr>
                           <td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
                             <div class="flex items-center">
-                              <div class="flex-shrink-0 w-10 h-10"><img class="w-10 h-12 rounded-lg" src="{{$tema->img ? asset($tema->img) : asset('img/el-principito.jpg')}}" alt=""></div>
+                              <div class="flex-shrink-0 w-10 h-10"><a href="{{route('admin.temas.show',$tema)}}"><img class="w-10 h-12 rounded-lg" src="{{$tema->img ? asset($tema->img) : asset('img/el-principito.jpg')}}" alt=""></a></div>
                               <div class="ml-4">
-                                <div class="text-sm font-medium leading-5 text-gray-900">{{$tema->name}}</div>
+                                <div class="text-sm font-medium leading-5 text-gray-900"><a href="{{route('admin.temas.show',$tema)}}">{{$tema->name}}</a></div>
 
                               </div>
                             </div>
