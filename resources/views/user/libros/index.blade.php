798a79839689572bccd67835ad5f2412875af19d
Daniel GG <daniel.gonzalez.garcia@iesdonana.org>
2022-12-15 15:20:31 +0100

    cambios estilos barra de busqueda

--- resources/views/user/libros/index.blade.php
+++ resources/views/user/libros/index.blade.php
@@ -6,10 +6,12 @@
   </x-slot>
 
   <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg  border-2 border-blue-400 " style="min-height: 80vh;">
 
-        <div x-data="ordenar" class="px-4 ">
+        <div class="flex flex-col md:flex-row  my-1 ">
+
+        <div x-data="ordenar" class="px-5 ">
 
           {{-- {{Request::getQueryString()}} --}}
           {{-- {{Request::input('sortBy')}} --}}
@@ -18,7 +20,7 @@
           <form id="ordenar" action="{{route('libros')}}">
 
             <input type="text" class="hidden" name="search" value="{{Request::input('search')}}">
-            <label for="nota" class="block mb-2 text-sm font-medium text-gray-900"></label>
+            <label for="nota" class="block mb-1 text-sm font-medium text-gray-900"></label>
             <select x-on:change="ordenar" name="sortBy" id="sortBy" class="px-12 bg-blue-50 border border-blue-200 text-gray-900 text-sm rounded-lg focus:ring-blue-300 focus:border-blue-300 block  p-2.5 ">
 
 
@@ -45,25 +47,28 @@
           </form>
 
         </div>
+<div class="mx-1">
 
-        <form id="formSearch" action="{{route('libros')}}"> 
+  <form id="formSearch" action="{{route('libros')}}"> 
+    
+    <input type="text" class="hidden" name="sortBy" value="{{Request::input('sortBy')}}">
+    
+    <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative w-72">
+      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+      </div>
+      <input value="{{Request::input('search')}}" name="search" type="search" id="search" class="mt-1 block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-blue-50 focus:ring-blue-300 focus:border-blue-300"  placeholder="Search ">
+      {{-- <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> --}}
+      <button type="submit" class="absolute top-0 right-0 p-3 text-sm font-medium text-white bg-blue-400 rounded-r-lg border border-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300">
+        <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        <span class="sr-only">Search</span>
+      </button>
+    </div>
+  </form>
+</div>
 
-          <input type="text" class="hidden" name="sortBy" value="{{Request::input('sortBy')}}">
-
-          <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-          <div class="relative">
-              <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-              </div>
-              <input value="{{Request::input('search')}}" name="search" type="search" id="search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-blue-50 focus:ring-blue-300 focus:border-blue-300"  placeholder="Search ">
-              {{-- <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> --}}
-              <button type="submit" class="absolute top-0 right-0 p-4 text-sm font-medium text-white bg-blue-400 rounded-r-lg border border-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                <span class="sr-only">Search</span>
-            </button>
-            </div>
-      </form>
-
+    </div>
         <div class="p-6 bg-white border-b border-gray-200">
           {!! $libros->appends(["sortBy" => $sortBy,"search" => $search,]) !!}
 
