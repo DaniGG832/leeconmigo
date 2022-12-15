664db3f0cf1b78beee7fefadcd39ce75162285c2
Daniel GG <daniel.gonzalez.garcia@iesdonana.org>
2022-12-15 17:27:10 +0100

    libros por autores

---
+++ resources/views/user/autores/show.blade.php
@@ -0,0 +1,44 @@
+<x-invitado>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          {{ __('Libros infantiles ' . $edad->descripcion) }}
+      </h2>
+  </x-slot>
+
+  <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg border-2 border-blue-400">
+            
+              <div class="p-6 bg-white border-b border-gray-200">
+                
+                {{ $libros->links() }}
+                
+                <div class="container mx-auto">
+                  <div class="flex flex-wrap -mx-4">
+
+                        @foreach ($libros as $libro)
+
+                        @include('user.libros._libro')
+
+                        @endforeach
+
+                        
+        
+                      </div>
+                    </div>
+
+
+                    {{-- ------------------------------- --}}
+                    
+                       
+                 
+
+
+                </div>
+                
+
+              </div>
+          </div>
+      </div>
+  </div>
+</x-invitado>
