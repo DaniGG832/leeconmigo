a4efa63cc11a637341df39c3ab5ae9979f7c9849
Daniel GG <daniel.gonzalez.garcia@iesdonana.org>
2022-12-15 15:30:04 +0100

    cambios estilos vista libros

--- resources/views/user/libros/index.blade.php
+++ resources/views/user/libros/index.blade.php
@@ -9,7 +9,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg  border-2 border-blue-400 " style="min-height: 80vh;">
 
-        <div class="flex flex-col md:flex-row  my-1 ">
+        <div class="flex flex-col justify-between md:flex-row  my-1 ">
 
         <div x-data="ordenar" class="px-5 ">
 
@@ -54,7 +54,7 @@
     <input type="text" class="hidden" name="sortBy" value="{{Request::input('sortBy')}}">
     
     <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div class="relative w-72">
+    <div class="relative w-72 mr-4">
       <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
       </div>
