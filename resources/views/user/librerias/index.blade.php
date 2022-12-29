cd4d471c753088399e82bb71574395e8082b5321
Daniel GG <daniel.gonzalez.garcia@iesdonana.org>
2022-12-29 13:19:16 +0100

    mostrar lista librerias

--- resources/views/user/librerias/index.blade.php
+++ resources/views/user/librerias/index.blade.php
@@ -12,17 +12,80 @@
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200" style="min-height: 25em">
 
-          <div x-data="librerias" class="flex flex-row">
-            <div class="w-96 h-96 m-3 border bg-blue-200"></div>
-            <div class="w-96 h-96 m-3" id="map"></div>
-          </div>
 
+          <p id="theError"></p>
+          <div x-data="librerias" class="flex xl:flex-row flex-col-reverse" style="min-height: 25em">
+            
+            <div class="xl:w-2/3">
+
+              <section class="relative bg-blueGray-50 ">
+                <div class="w-full pr-2">
+                  <div class="relative flex flex-col min-w-0 break-words w-full mb-4 shadow-lg rounded 
+                  bg-blue-700 text-white">
+                    <div class="block w-full overflow-x-auto rounded-t">
+                      <table class="items-center w-full bg-transparent border-collapse">
+                        <thead>
+                          <tr>
+                            <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blue-800 text-blue-300 border-blue-700">Nombre</th>
+                            <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blue-800 text-blue-300 border-blue-700">Direccion</th>
+                            <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blue-800 text-blue-300 border-blue-700">ciudad</th>
+                            <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blue-800 text-blue-300 border-blue-700">Código postal</th>
+                            <th class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blue-800 text-blue-300 border-blue-700">Provincia</th>
+                          </tr>
+                        </thead>
+                
+                        <tbody>
+
+                          @foreach ($librerias as $libreria)
+                          <tr>
+                            <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                              <img src="{{$libreria->img ? asset($libreria->img) : asset('img/el-principito.jpg')}}" class="h-12 w-12 bg-white rounded-t border" alt="...">
+                              <span class="ml-3 font-bold text-white"> {{$libreria->nombre}} </span></th>
+                            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">{{$libreria->direccion}}</td>
+                            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                              {{$libreria->ciudad}}
+                            </td>
+                            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                              {{$libreria->cod_postal}}
+                            </td>
+                            <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                              {{$libreria->provincia->nombre}}
+                            </td>
+                            
+                            
+                        
+                          </tr>
+
+
+                          @endforeach
+                        
+                
+                          
+                
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+                    
+                </section>
+
+
+              
+            </div>
+            <div class="xl:w-1/3 w-full h-96 xl:h-auto flex flex-col">
+
+              <div class=" w-full h-full" id="map"></div>
+              <div class="flex flex-col text-center">
+    
+                <p class="text-xs">Su ubicación actual es <span id="spanLatitude"></span>, <span id="spanLongitude"></span> grados.</p>
+              </div>
+            </div>
+          </div>
+          
           {{-- {{$librerias}} --}}
 
-          <h1>JnjSite.com: Javascript Geolocation</h1>
-          <p>Your current location is latitude <span id="spanLatitude"></span> degrees and longitude <span id="spanLongitude"></span> degrees.</p>
-          <p id="theError"></p>
-
+          
         </div>
       </div>
     </div>
