a595b9848bdf3b9682a92ba1fa978a73297b2374
Daniel GG <daniel.gonzalez.garcia@iesdonana.org>
2022-12-30 17:58:59 +0100

    el registro se realiza a traves de Laravel Breeze

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,11 @@
 
-<footer class="p-4 bg-blue-300  shadow md:px-6 md:py-8 dark:bg-gray-900">
+<footer class="p-4 bg-blue-300  shadow md:px-6 md:py-8">
   <div class="sm:flex sm:items-center sm:justify-between">
       <a href="/" class="flex items-center mb-4 sm:mb-0">
           <img src="{{asset('img/logo/logo3.png')}}" class="mr-3 h-8" alt="LeeConmigo Logo" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">LeeConmigo</span>
+          <span class="self-center text-2xl font-semibold whitespace-nowrap">LeeConmigo</span>
       </a>
-      <ul x-data="footer" class="flex flex-wrap items-center mb-6 text-sm text-gray-600 sm:mb-0 dark:text-gray-400">
+      <ul x-data="footer" class="flex flex-wrap items-center mb-6 text-sm text-gray-600 sm:mb-0">
           <li>
               <a href="{{route('contactanos.index')}}" class="mr-4 hover:underline md:mr-6 ">Contáctanos</a>
           </li>
@@ -20,7 +20,7 @@
           </li>
       </ul>
   </div>
-  <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-  <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" class="hover:underline">LeeConmigo™</a>. Todos los derechos reservados.
+  <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+  <span class="block text-sm text-gray-500 sm:text-center">© 2022 <a href="/" class="hover:underline">LeeConmigo™</a>. Todos los derechos reservados.
   </span>
 </footer>
