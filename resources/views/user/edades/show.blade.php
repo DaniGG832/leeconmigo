372a2a5d824e54c6fca45102dc99ba6868dc0bd9
Daniel GG <daniel.gonzalez.garcia@iesdonana.org>
2023-01-14 00:57:56 +0100

    corregidos algunos errores encontrados al hacer la documentacion

--- resources/views/user/edades/show.blade.php
+++ resources/views/user/edades/show.blade.php
@@ -6,10 +6,10 @@
   </x-slot>
 
   <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg border-2 border-blue-400">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 min-h-[22em]">
+          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg border-2 border-blue-400 min-h-[15em]">
             
-              <div class="p-6 bg-white border-b border-gray-200">
+              <div class="p-6 bg-white border-b border-gray-200 ">
                 <div class="mb-5">
     
                   <a href="{{route('edades.index')}}" class="  border border-blue-600 hover:bg-blue-600 p-2  text-blue-600 hover:text-blue-50 rounded-lg">
