ae2fe03fa934f176673cdb3b721bc602d1c4e410
Daniel GG <daniel.gonzalez.garcia@iesdonana.org>
2023-01-15 00:53:02 +0100

    cambiar fecha y hora  en criticas

--- resources/views/user/criticas/_critica.blade.php
+++ resources/views/user/criticas/_critica.blade.php
@@ -15,9 +15,10 @@
       {{$critica->user->name}}
       
       <a href="{{route('votos.usuario',[$critica->user,$libro])}}">
-        <p class="text-xs text-gray-400"> {{$critica->user->votaciones->count()}} Votaciones <span class="text-xs hover:text-blue-800 decoration-blue-800 hover:underline">( Ver )</span></p>
+        <p class="text-xs text-gray-400"> {{$critica->user->votaciones->count()}} Votos <span class="text-xs hover:text-blue-800 decoration-blue-800 hover:underline">( Ver )</span></p>
       </a>
       <p class="text-xs text-gray-400"> {{$critica->user->criticas->count()}} Criticas</p>
+      <p class="text-xs text-gray-400 mt-1"> {{($critica->created_at)?($critica->created_at)->format('d-m-Y H:i:s'): ''}}</p>
     </h3>
     <div class="text-gray-400 rounded-lg  p-1.5 ml-auto inline-flex items-center">
       <p class="text-2xl text-red-500  p-3 border border-blue-400 rounded-lg">{{($critica->votoLibro($libro)) ? $critica->votoLibro($libro) : '-'}}</p>
@@ -27,7 +28,7 @@
   <div class="p-6 space-y-6 ">
     <h3 class="text-gray-900 text-sm md:text-xl font-semibold ml-3">
       {{$critica->titulo}}
-      <p class="text-xs text-gray-400"> {{isset($critica->created_at) ? $critica->created_at->format('d-m-Y H:i:s') :''}}</p>
+    {{--   <p class="text-xs text-gray-400"> {{isset($critica->created_at) ? $critica->created_at->format('d-m-Y H:i:s') :''}}</p> --}}
 
     </h3>
     <p class="text-gray-500 text-xs md:text-sm leading-relaxed ">
