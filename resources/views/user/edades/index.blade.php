c6874435f37d364dd63f8ebdc637844d7baf98ec
Daniel GG <daniel.gonzalez.garcia@iesdonana.org>
2023-01-14 17:40:59 +0100

    cambiar vista edades

--- resources/views/user/edades/index.blade.php
+++ resources/views/user/edades/index.blade.php
@@ -6,7 +6,7 @@
   </x-slot>
 
   <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 min-h-[40em]">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 min-h-[22em]">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg border-2 border-blue-400">
             
               <div class="p-6 bg-white border-b border-gray-200">
@@ -17,18 +17,18 @@
                         @foreach ($edades as $edad)
 
                         <div class="p-1 w-96">
-                          <div class="m-1 border-blue-100 rounded-md border p-2">
+                          <div class="m-1 border-blue-100 rounded-md border p-2 bg-blue-200">
                             <a href="{{route('edades.show',$edad)}}" class="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-                            <div class="relative pb-56 overflow-hidden">
+                            <div class="relative  bg-blue-50">
                               {{-- <div class="rounded-full absolute z-50 bg-red-600">7.5</div> --}}
-                              <img class="absolute inset-0 h-56 w-full object-cover" src="{{$edad->img ? asset($edad->img) : asset('img/book-1977235_960_720.webp')}}" alt="">
+                              {{-- <img class="absolute inset-0 h-56 w-full object-cover" src="{{$edad->img ? asset($edad->img) : asset('img/book-1977235_960_720.webp')}}" alt=""> --}}
+                              <div class="p-4 text-center text-blue-900">
+                              
+                                <h2 class="mt-2 mb-2  font-bold">{{$edad->descripcion}}</h2>
+                              
+                              </div>
                             </div>
                         </a>
-                            <div class="p-4 text-center">
-                            
-                              <h2 class="mt-2 mb-2  font-bold">{{$edad->descripcion}}</h2>
-                            
-                            </div>
                           </div>
                           </div>
 
